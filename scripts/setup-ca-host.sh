ed202e1bfa8f4eb591631fb219ba43ee4202b43c
Dmitry Sylkin <roysa99@gmail.com>
2025-10-04 02:27:24 +0300

    build: conf domain

--- scripts/setup-ca-host.sh
+++ scripts/setup-ca-host.sh
@@ -8,6 +8,14 @@ cd $BASEDIR
 
 BASEDIR=$PWD
 
+# Load environment variables from project root .env if present
+if [ -f "$BASEDIR/.env" ]; then
+  set -a
+  # shellcheck source=/dev/null
+  . "$BASEDIR/.env"
+  set +a
+fi
+
 DOCKERDATADIR=$BASEDIR/data
 SCRIPTDATADIR=$BASEDIR/scripts/data
 
@@ -18,7 +26,17 @@ cp -f $DOCKERDATADIR/certs/ca.crt $DOCKERDATADIR/html/ca.crt
 
 cp -i $SCRIPTDATADIR/ca-vhost.conf $DOCKERDATADIR/conf/local-ca.conf
 
+# If CA_CERT_DOMAIN is set, update the server_name in the destination config
+if [ -n "${CA_CERT_DOMAIN:-}" ]; then
+  DEST_FILE="$DOCKERDATADIR/conf/local-ca.conf"
+  # Escape slashes and ampersands in replacement to keep sed safe
+  REPLACEMENT=${CA_CERT_DOMAIN//\//\\/}
+  REPLACEMENT=${REPLACEMENT//&/\\&}
+  sed -i -E "s/nginx-proxy\\.local/${REPLACEMENT}/g" "$DEST_FILE"
+fi
+
 docker compose restart nginx
 
-echo -e "\nCA virtual host setup complete. Please add '127.0.0.1 nginx-proxy.local to the /etc/hosts file.'\n"
-echo -e "\nServer URL: http://nginx-proxy.local\n"
+DOMAIN="${CA_CERT_DOMAIN:-nginx-proxy.local}"
+echo -e "\nCA virtual host setup complete. Please add '127.0.0.1 ${DOMAIN} to the /etc/hosts file.'\n"
+echo -e "\nServer URL: http://${DOMAIN}\n"
